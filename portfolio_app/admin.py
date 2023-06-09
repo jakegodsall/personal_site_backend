ea5ac8d90337fb985eaca3294a3ea707bd4d76dc
jakegodsall <jake.edward.godsall@gmail.com>
2023-06-09 09:53:32 +0100

    added viewsets to url.py and added Language and Framework to admin site

--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 from .api.models.project import Project
-from .api.models.skill import Skill
+from .api.models.language import Language
+from .api.models.framework import Framework
 
 admin.site.register(Project)
-admin.site.register(Skill)
+admin.site.register(Language)
+admin.site.register(Framework)
--- portfolio_app/api/urls.py
+++ portfolio_app/api/urls.py
@@ -1,8 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import ProjectViewSet
+from .views import ProjectViewSet, LanguageViewSet, FrameworkViewSet
 
 router = DefaultRouter()
 router.register('project', ProjectViewSet, basename='project')
+router.register('language', LanguageViewSet, basename='language')
+router.register('framework', FrameworkViewSet, basename='framework')
 
 urlpatterns = router.urls
