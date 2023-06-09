7f65d12afdf0ee5839a1dbd158877bb782f7eb04
jakegodsall <jake.edward.godsall@gmail.com>
2023-06-09 09:56:15 +0100

    made migrations of new models

--- portfolio_app/api/models/framework.py
+++ portfolio_app/api/models/framework.py
@@ -1,12 +1,8 @@
 from django.db import models
 
-from .language import Language
-
 
 class Framework(models.Model):
     name = models.CharField(max_length=128, unique=True, blank=False)
-    language = models.ForeignKey(
-        Language, on_delete=models.CASCADE, related_name="language")
 
     def __str__(self):
         return self.name
